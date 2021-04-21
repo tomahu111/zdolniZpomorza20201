651c6aacfafc16fd2c02c31c3d53722936a2ad41
KrystianoXPL <KrystianoXPL@gmail.com>
2021-04-21 12:49:30 +0200

    Poprawa serwera

--- epserver.py
+++ epserver.py
@@ -1,46 +1,46 @@
 import socket
 import threading
 
-RUNNING = False
-PORT = 37234
-HOST = 0
-BUFFER = 1024
-def setup(ip=None, port=37234):
-    if ip is None:
-        ip = socket.gethostbyname(socket.gethostname())
-    global PORT, HOST, BUFFER
-    PORT = 37234
-    HOST = ip
-    BUFFER = 1024
-    
-    #hostname = socket.gethostname()
-    #HOST = socket.gethostbyname(hostname)
-def start():
-    global PORT,HOST,BUFFER,RUNNING
-    if RUNNING == False:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind((HOST, PORT))
-        server_socket.listen(5)
-
+class epserver:
+    RUNNING=False
+    def __init__(self,ip=None, port=37234):
+        if ip is None:
+            self.ip = socket.gethostbyname(socket.gethostname())
+        else:
+            self.ip = ip
+        self.PORT = 37234
+        self.HOST = self.ip
+        self.BUFFER = 1024
+        self.server_socket = None
+        self.sv_thread = None
+        
+        #hostname = socket.gethostname()
+        #HOST = socket.gethostbyname(hostname)
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.HOST, self.PORT))
+        self.server_socket.listen(5)
         def serverthread():
             print("Wątek z serwerem uruchomiony!")
-            while True:
-                client_socket, adr = server_socket.accept()
+            client_socket, adr = self.server_socket.accept()
+            info = "Witaj w ePaint 0.001".encode("utf8")
+            client_socket.send(info)
+            while self.RUNNING == True:
                 print('Połączenie z ', adr[0], 'Port: ', adr[1])
                 # FROM CLIENT
                 host_ip = client_socket.recv(BUFFER).decode("utf8")
                 print(host_ip)
+                
 
-                info = "Witaj w ePaint 0.001".encode("utf8")
-                client_socket.send(info)
-        sv = threading.Thread(target=serverthread)
-        sv.start()
-        RUNNING=True
-        return True
-    else:
-        return False
+        self.RUNNING=True
+        self.sv_thread = threading.Thread(target=serverthread)
+        self.sv_thread.start()
+
+    def stop(self):
+        self.server_socket.close()
+        self.RUNNING=False
     # info = (1, 2, 3, 4, 5, 6, 7, 8)
 
-        
+
         
     
